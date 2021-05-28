b1e70a6505739066c071ac7b54ed7b5c4f540d0d
Lucas Heller <lucas.heller@mercadolivre.com>
2021-05-28 15:41:54 -0300

    INITIAL COMMIT

---
+++ src/main/java/com/bootcamp/complementar/ComplementarApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.complementar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplementarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplementarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/complementar/ComplementarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.complementar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplementarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
